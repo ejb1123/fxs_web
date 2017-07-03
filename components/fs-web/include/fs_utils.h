6c41112c9482f1f23f76f19762a8e6050c6dbdd5
E.J. Bevenour <ejbevenour@live.com>
2017-07-02 21:25:52 -0400

    fixed git
    I hope

---
+++ components/fs-web/include/fs_utils.h
@@ -0,0 +1,26 @@
+﻿#include <ResourceManager.h>
+#pragma once
+namespace fs {
+	static char * StateToString(fwRefContainer<fx::Resource> resource) {
+		char * state;
+		switch (resource->GetState())
+		{
+		case fx::ResourceState::Started:
+			state = "started";
+			break;
+		case fx::ResourceState::Starting:
+			state = "starting";
+			break;
+		case fx::ResourceState::Stopped:
+			state = "stopped";
+			break;
+		case fx::ResourceState::Stopping:
+			state = "stopping";
+			break;
+		case fx::ResourceState::Uninitialized:
+			state = "uninitialized";
+			break;
+		}
+		return state;
+	}
+}
---
+++ components/fs-web/src/HttpHandles.cpp
@@ -0,0 +1,104 @@
+﻿#include "StdInc.h"
+#include <ServerInstanceBase.h>
+#include <HttpServerManager.h>
+#include <ClientRegistry.h>
+
+#include <ResourceManager.h>
+
+#include <VFSManager.h>
+
+#include <botan/base64.h>
+
+#include <json.hpp>
+#include <fs_utils.h>
+
+using json = nlohmann::json;
+
+static InitFunction initFunction([]()
+{
+	fx::ServerInstanceBase::OnServerCreate.Connect([](fx::ServerInstanceBase *instance)
+	{
+		// TODO: make instanceable
+		static auto instanceRef = instance;
+		static auto ivVar = instance->AddVariable<int>("sv_infoVersion", ConVar_ServerInfo, 0);
+		static auto maxClientsVar = instance->AddVariable<int>("sv_maxClients", ConVar_ServerInfo, 30);
+		auto epPrivacy = instance->AddVariable<bool>("sv_endpointPrivacy", ConVar_None, false);
+
+		// max clients cap
+		maxClientsVar->GetHelper()->SetConstraints(1, 32);
+
+		struct InfoData
+		{
+			json infoJson;
+			int infoHash;
+
+			InfoData()
+				: infoHash(0), infoJson({ { "server", "FXServer-pre" },{ "enhancedHostSupport", true },{ "resources",{} } })
+			{
+				Update();
+			}
+
+			void Update()
+			{
+				auto varman = instanceRef->GetComponent<console::Context>()->GetVariableManager();
+
+				infoJson["vars"] = json::object();
+
+				varman->ForAllVariables([&](const std::string& name, int flags, const std::shared_ptr<internal::ConsoleVariableEntryBase>& var)
+				{
+					// don't return more variable information
+					if (name == "sv_infoVersion" || name == "sv_hostname")
+					{
+						return;
+					}
+
+					infoJson["vars"][name] = var->GetValue();
+				}, ConVar_ServerInfo);
+
+				
+
+				infoJson["version"] = 0;
+
+				infoHash = static_cast<int>(std::hash<std::string>()(infoJson.dump()) & 0x7FFFFFFF);
+				infoJson["version"] = infoHash;
+
+				infoJson["resources"] = json::object();
+				infoJson["resources"]["hardcap"] = json::object();
+				
+				auto resman = instanceRef->GetComponent<fx::ResourceManager>();
+				resman->ForAllResources([&](fwRefContainer<fx::Resource> resource)
+				{
+					// we've already listed hardcap, no need to actually return it again
+					if (resource->GetName() == "hardcap")
+					{
+						return;
+					}
+					//if (resource->GetState == fx::ResourceState::)
+					infoJson["resources"][resource->GetName()] = json::object();
+					char* state = fs::StateToString(resource);
+					infoJson["resources"][resource->GetName()]["state"] = (state);
+				});
+
+
+
+				ivVar->GetHelper()->SetRawValue(infoHash);
+			}
+		};
+
+		static auto infoData = std::make_shared<InfoData>();
+		instance->GetComponent<fx::HttpServerManager>()->AddEndpoint("/fs-web", [=](const fwRefContainer<net::HttpRequest>& request, const fwRefContainer<net::HttpResponse>& response)
+		{
+			infoData->Update();
+			response->End(infoData->infoJson.dump());
+		});
+
+		instance->GetComponent<fx::HttpServerManager>()->AddEndpoint("/fs-web", [](const fwRefContainer<net::HttpRequest>&request,const fwRefContainer<net::HttpResponse>& response)
+		{
+			if (request->GetRequestMethod()=="POST")
+			{
+
+			}
+		});
+	}, 1500);
+}
+);
