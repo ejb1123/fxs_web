3e75dd54ff4cdfe9f5232b06a0b272296f20f7ee
E.J. Bevenour <ejb1123@protonmail.com>
2017-07-30 13:03:12 -0400

    Made paths parsed with std::regex

--- components/fs-web/src/HttpHandles.cpp
+++ components/fs-web/src/HttpHandles.cpp
@@ -32,78 +32,90 @@ SOFTWARE.
 #include "ServerData.h"
 #include "HttpServer.h"
 #include <fstream>
+#include <regex>
 
 
 using json = nlohmann::json;
 bool isAuthed(fwRefContainer<net::HttpRequest> const request, fwRefContainer<net::HttpResponse> const response, std::string rcon_password);
-
+static bool doesmatch(const std::string &path, std::smatch &matches, const std::regex &regex) {
+	return std::regex_match(path.begin(), path.end(), matches, regex, std::regex_constants::match_default);
+}
 static InitFunction initFunction([]()
 {
 	fx::ServerInstanceBase::OnServerCreate.Connect([](fx::ServerInstanceBase *instance)
 	{
+		//TODO: add update checker
 		static auto console = instance->GetComponent<console::Context>();
 		static auto infoData = std::make_shared<InfoData>(instance);
 		static auto playerData = std::make_shared<PlayerData>(instance);
 		static auto serverData = std::make_shared<ServerData>(instance);
 		static auto resourceData = std::make_shared<ResourceData>(instance);
 		trace("creating fs server", "fs-web");
+		static std::regex playersNETID("^\/fsdata\/players\/(\\d{1,3})", std::regex::ECMAScript);
+		static std::regex players("^\/fsdata\/players", std::regex::ECMAScript);
+		static std::regex server("^\/fsdata\/server", std::regex::ECMAScript);
+		static std::regex resources("^\/fsdata\/resources", std::regex::ECMAScript);
+		static std::regex logregex("^\/fsdata\/log",std::regex::ECMAScript);
+		static std::regex logAction("^\/fsdata\/log\/actions\/(.*)", std::regex::ECMAScript);
+		static std::smatch l;
 		instance->GetComponent<fx::HttpServerManager>()->AddEndpoint("/fsdata", [=](const fwRefContainer<net::HttpRequest>&request, const fwRefContainer<net::HttpResponse>& response)
 		{
 			if (fs::isAuthed(request, response, instance)) {
 				if (request->GetRequestMethod() == "GET")
 				{
-					if (request->GetPath().substr(7).length() > 0)
-					{
-						auto morepath = request->GetPath().substr(7);
-						if (morepath == "/players")
-						{
-							playerData->update();
-							response->SetStatusCode(200);
-							response->End(playerData->jsonData.dump());
-						}
-						else if (morepath == "/server")
-						{
-							serverData->update();
-							response->SetStatusCode(200);
-							response->End(serverData->info.dump());
-						}
-						else if (morepath == "/resources")
-						{
-							resourceData->update();
-							response->End(resourceData->info.dump());
-						}
-						else if (morepath == "/log")
-						{
-							std::string line;
-							std::ifstream logFile;
-							logFile.open(MakeRelativeCitPath(L"CitizenFX.log").c_str());
-							std::string log;
+					if (doesmatch(request->GetPath(), l, players)) {
+						playerData->update();
+						response->SetStatusCode(200);
+						response->End(playerData->jsonData.dump());
+					}
 
-							while (logFile.is_open())
-							{
-								while (std::getline(logFile,line))
-								{
-									log += line;
-								}
-								logFile.close();
-							}
-							response->SetHeader("Content-Type", "text/plain");
-							response->End(log);
-						}
-						else if(morepath=="/log/actions/clear")
+					if (doesmatch(request->GetPath(), l, playersNETID)) {
+						//TODO: add /players/{net_id}
+					}
+
+					//TODO: add /players/{net_id}/actions
+
+					if (doesmatch(request->GetPath(), l, server)) {
+						serverData->update();
+						response->SetStatusCode(200);
+						response->End(serverData->info.dump());
+					}
+
+					if (doesmatch(request->GetPath(), l, resources)) {
+						resourceData->update();
+						response->End(resourceData->info.dump());
+					}
+
+
+					if (doesmatch(request->GetPath(), l, logregex))
+					{
+						std::string line;
+						std::ifstream logFile;
+						logFile.open(MakeRelativeCitPath(L"CitizenFX.log").c_str());
+						std::string log;
+
+						while (logFile.is_open())
 						{
+							while (std::getline(logFile, line))
+							{
+								log += line;
+							}
+							logFile.close();
+						}
+						response->SetHeader("Content-Type", "text/plain");
+						response->End(log);
+					}
+					if (doesmatch(request->GetPath(), l, logAction))
+					{
+						if (l[1] == "clear") {
 							std::fstream logFile;
-							logFile.open(MakeRelativeCitPath(L"CitizenFX.log").c_str(),std::ios::out | std::ios::trunc);
+							logFile.open(MakeRelativeCitPath(L"CitizenFX.log").c_str(), std::ios::out | std::ios::trunc);
 							logFile.close();
 							response->End("");
 						}
-						else
-						{
-							response->SetStatusCode(404);
-							response->End("");
-						}
-						//if (morepath.substr())
 					}
+
+					return;
 				}
 			}
 		});
